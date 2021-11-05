c0ef0a7c6ef109cad846457bd3dfcf024d5824e8
RaiderNationBuilder <71417297+RaiderNationBuilder@users.noreply.github.com>
2021-11-05 11:50:39 -0500

    all classes set up

---
+++ index.js
@@ -0,0 +1,99 @@
+const inquirer = require('inquirer')
+const Intern = require('./lib/Intern')
+
+const team = []
+
+
+// var fakehtml = `
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>Document</title>
+// </head>
+// <body>
+  
+// </body>
+// </html>
+// `
+
+function mainQuestions() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "firstname",
+            message: "Whats your first name?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Whats your email address?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "Whats your id?"
+        },
+        {
+            type: "list",
+            name: "type",
+            message: "Which employee would you like to make?",
+            choices: [
+                "Intern",
+                "Manager",
+                "Engineer"
+            ]
+        }   
+    ]).then(function(answer) {
+        
+        if (answer.type === "Intern") {
+            internQuestions(answer)
+        } else if (answer.type === "Manager") {
+            managerQuestions()
+        } else if (answer.type === "Engineer") {
+            engineerQuestions()
+        }
+    }) 
+}
+
+function internQuestions(previousData) {
+    console.log("Time to ask intern questons.")
+    // inquiere prompt
+    inquirer.prompt({
+        type: "input",
+        name: "school",
+        message: "whats your school?"
+    }) .then(function(answers) {
+        console.log(answers, previousData)
+        // make the inter
+        var intern = new Intern(previousData.name, previousData.id, previousData.email, answers.school)
+        team.push(intern)
+        addAnother()
+    })
+}
+
+function managerQuestions() {
+    console.log("Time to ask manager questons.")
+}
+
+function engineerQuestions() {
+    console.log("Time to ask engineer questons.")
+}
+
+function addAnother() {
+    inquirer.prompt({
+        type: "confirm",
+        name: "addAnother",
+        message: "Would you like to add another employee?"
+    }).then(function(answer) {
+if (answer.addAnother) {
+    mainQuestions()
+} else {
+    console.log("time to make HTML")
+}
+    })
+}
+
+mainQuestions()
